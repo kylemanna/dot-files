554143721edd1bbc00285559e992bce648511900
Kyle Manna <kyle@kylemanna.com>
2023-06-03 23:37:31 +0000

    fish: Use `exa` for `ls` only if available
    * Only do it if it's available.

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,10 @@
+type -q exa
+set exa_available $status
+
 function ls --wraps='exa -s modified' --description 'alias ls exa -s modified'
-  exa -s modified $argv
-        
+  if test $exa_available -eq 0
+    exa -s modified $argv
+  else
+    eval (type -P ls) --color -t $argv
+  end
 end
