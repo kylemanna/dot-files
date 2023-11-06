377caddfcfc337133fc857459696c6d80c98483d
Kyle Manna <kyle@kylemanna.com>
2023-11-06 00:25:18 -0600

    fish: Tweak auto-ssh-agent and ls
    * More tweaks.

--- .config/fish/functions/auto-ssh-agent.fish
+++ .config/fish/functions/auto-ssh-agent.fish
@@ -1,3 +1,4 @@
 function auto-ssh-agent --description 'automatically set SSH_AUTH_SOCK to most recent agent'
  set -x SSH_AUTH_SOCK (fd -p /ssh-\.\*\/agent\. /tmp)
+ [ -z "$SSH_AUTH_SOCK" ] && eval (ssh-agent -c)
 end
--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,9 +1,14 @@
 type -q lsd
 set lsd_available $status
 
+type -q eza
+set eza_available $status
+
 function ls --wraps='lsd' --description 'LS Deluxe'
   if test $lsd_available -eq 0
-    lsd -t $argv
+    lsd -tr $argv
+  else if test $eza_available -eq 0
+    eza -t modified $argv
   else
     eval (type -P ls) --color -t $argv
   end
