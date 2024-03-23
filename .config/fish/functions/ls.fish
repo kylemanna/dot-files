a3afdef8521b2845afd7f97f4b23fa7694e6d225
Kyle Manna <kyle@kylemanna.com>
2024-03-23 11:22:16 -0500

    fish: ls: Handle spaces better
    * Quote all the things so they don't break.

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -5,9 +5,9 @@ type -q eza
 set eza_available $status
 
 function ls --wraps='lsd' --description 'LS Deluxe'
-  if test $lsd_available -eq 0
+  if test "$lsd_available" -eq 0
     lsd -tr $argv
-  else if test $eza_available -eq 0
+  else if test "$eza_available" -eq 0
     eza -t modified $argv
   else
     eval (type -P ls) --color -t $argv
